674e2b2786af6876b2bdfe965b289f4a44bf86df
Tomas Svojanovsky <tomas.svojanovsky33@gmail.com>
2023-04-29 19:19:56 +0200

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { IBM_Plex_Sans } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
+const ibmPlexSans = IBM_Plex_Sans({ subsets: ['latin'], weight: ["300", "400", "500", "600", "700"] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -15,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${ibmPlexSans.className} w-screen md:min-h-screen overflow-x-hidden`}>{children}</body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,116 +1,88 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import Image from "next/image";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    return (
+        <div className={"grid place-items-center md:bg-[url('/images/bg-main-tablet.png')] lg:bg-[url('/images/bg-main-desktop.png')] md:bg-[-300px_-250px] lg:bg-center"}>
+            <Image className={"hidden md:inline lg:hidden absolute right-0 top-[-2rem]"} src={"/images/bg-pattern-1.svg"} width={266} height={400} alt={"Pattern 1"}/>
+            <Image className={"hidden lg:inline absolute right-0 top-[-2rem]"} src={"/images/bg-pattern-1.svg"} width={312} height={468} alt={"Pattern 1"}/>
+
+            <main className={"pt-10 pb-20 md:px-10 text-black overflow-hidden lg:max-w-[69.375rem]"}>
+                <div className={"relative px-6 md:px-0"}>
+                    <Image src={"/images/logo.svg"} width={146} height={32} alt={"logo"}/>
+                    <div className={"absolute top-[-12rem] right-[-7.25rem] md:hidden"}>
+                        <div className={"relative w-[17.563rem] h-[30rem]"}>
+                            <Image className={"scale-[2.5]"} src={"/images/bg-main-mobile.png"} fill alt={"logo"}/>
+                        </div>
+                    </div>
+
+
+                    <h1 className={`font-bold text-[2.5rem] md:text-[4rem]
+                leading-[3rem] md:leading-[4rem] tracking-[-0.028rem] md:tracking-[-0.045rem] mt-16 md:mt-24 md:pr-[10.563rem] max-w-[51.563rem]`}>We make your music
+                        sound extraordinary.</h1>
+
+                    <p className={"leading-[1.625rem] md:leading-[1.75rem] md:text-lg mt-5 md:pr-[10.863rem] max-w-[51.563rem]"}>A system audio equalizer specifically designed for Android and
+                        iOS. Freely tune the way your music sounds
+                        with a professional grade parametric EQ & volume mixer. Control bass, mids, treble, gain control,
+                        reverb, and more!</p>
+                </div>
+
+                <div className={"relative bg-black px-12 mt-[10.375rem] -z-10 md:h-[37.5rem]"}>
+                    <div className="absolute top-[-6rem] left-0  right-0 flex justify-center md:justify-start md:ml-16">
+                        <div className={"relative h-[26.875rem] w-[13.063rem] md:h-[34.75rem] md:w-[16.875rem]"}>
+                            <Image src={"/images/illustration-app.png"} fill alt={"Illustration app"}/>
+                        </div>
+                    </div>
+                    <Image className={"md:mx-auto"} src={"/images/bg-pattern-2.svg"} width={280} height={420} alt={"Pattern 2"}/>
+                </div>
+
+                <div className={"text-gray bg-orange px-10 py-12 md:px-12 lg:px-14 rounded-xl mt-[-2rem] md:mt-[-26rem] md:ml-[14.938rem] md:w-[24.938rem] lg:ml-[35.563rem] lg:mt-[-30rem]"}>
+                    <h2 className={"text-3xl font-bold leading-[2.5rem]"}>Premium EQ</h2>
+
+                    <p className={"text-lg leading-[1.75rem] mt-3"}>Get expert-level control with a robust equalizer, volume mixer, and spatial audio. Take your
+                        listening experience to a whole new level and access all our incredible features!</p>
+
+                    <div className={"flex gap-4 items-center mt-9"}>
+                        <div className={"text-6xl font-bold"}>$4</div>
+                        <div className={"tracking-[-0.013rem]"}>/ month</div>
+                    </div>
+
+                    <div className={"flex flex-col gap-4 text-lg tracking-[-0.011rem] mt-8"}>
+                        <button className={"rounded-xl flex gap-2 justify-center items-center relative h-[3.813rem] w-full bg-black py-4 px-14"}>
+                            <Image src={"/images/icon-apple.svg"} width={20} height={17} alt={"ios"}/>
+                            <span>iOS Download</span>
+                        </button>
+                        <button className={"text-black rounded-xl flex gap-2 justify-center items-center relative h-[3.813rem] w-full bg-gray py-4 px-14"}>
+                            <Image src={"/images/icon-android.svg"} width={20} height={17} alt={"android"}/>
+                            <span>Android Download</span>
+                        </button>
+                    </div>
+                </div>
+
+                <div className={"relative px-6 mt-16 lg:mt-32 flex flex-col md:flex-row md:items-center md:justify-between"}>
+                    <div className={"max-w-[22.875rem] lg:max-w-none flex flex-col lg:flex-row items-start lg:gap-32"}>
+                        <Image src={"/images/logo.svg"} width={146} height={32} alt={"logo"}/>
+                        <div className={"leading-[1.625rem] mt-8 lg:mt-0"}>
+                            <div>
+                                <div>
+                                    All rights reserved © Equalizer 2021
+                                </div>
+
+                                <div>
+                                    Have any problems? Contact us via social media or email us at
+                                    <span className={"hidden md:inline font-bold"}>&nbsp;equalizer@example.com</span>
+                                </div>
+                            </div>
+                            <div className={"font-bold md:hidden"}>equalizer@example.com</div>
+                        </div>
+                    </div>
+
+                    <div className={"flex gap-5 mt-16 lg:mt-0"}>
+                        <Image src={"/images/icon-facebook.svg"} width={20} height={20} alt={"Facebook"}/>
+                        <Image src={"/images/icon-instagram.svg"} width={20} height={20} alt={"Instagram"}/>
+                        <Image src={"/images/icon-twitter.svg"} width={20} height={20} alt={"Twitter"}/>
+                    </div>
+                </div>
+            </main>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    );
 }
